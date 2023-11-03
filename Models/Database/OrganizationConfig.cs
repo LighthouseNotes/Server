6d3f4c74cc19235d396c7b34ba87bb814e820857
Ben Davies <ben@daviesfamily.uk>
2023-11-03 14:59:20 +0000

    Add S3 Network Encryption option

--- Models/Database/OrganizationConfig.cs
+++ Models/Database/OrganizationConfig.cs
@@ -9,6 +9,7 @@ public class OrganizationConfiguration : Base
     public virtual Organization Organization { get; set; } = null!;
     public required string S3Endpoint { get; set; }
     public required string S3BucketName { get; set; }
+    public required bool S3NetworkEncryption { get; set; } = true;
     public required string S3AccessKey { get; set; }
     public required string S3SecretKey { get; set; }
 }
