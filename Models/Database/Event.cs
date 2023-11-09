c82bd1d1ed1a029d20dd6d2be17078c5e49080a1
Ben Davies <ben@daviesfamily.uk>
2023-11-09 20:53:14 +0000

    Create event table

---
+++ Models/Database/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Text.Json;
+
+namespace LighthouseNotesServer.Models.Database;
+
+public class Event
+{
+    [Key]
+    public long Id { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public required JsonDocument Data { get; set; }
+    public required string EventType { get; set; }
+    public virtual Organization? Organization { get; set; }
+    public virtual User? User { get; set; }
+
+    public void Dispose() => Data?.Dispose();
+}
