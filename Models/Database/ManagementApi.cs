e5a0663124ecdac762a4d24a9f7ba25baf02db26
Ben Davies <ben@daviesfamily.uk>
2023-12-15 12:04:42 +0000

    Remove ManagementApi.cs

--- Models/Database/ManagementApi.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LighthouseNotesServer.Models.Database;
-
-public class ManagementApi : Base
-{
-    public int Id { get; set; }
-    public required string AccessToken { get; set; }
-    public DateTime Expires { get; set; }
-}
