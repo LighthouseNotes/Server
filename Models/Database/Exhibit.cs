0c0aeec10110d7c052078b79087971b84c23defa
Ben Davies <ben@daviesfamily.uk>
2024-01-21 20:41:24 +0000

    Fix exhibit DB relations and remove lazy loading

--- Models/Database/Exhibit.cs
+++ Models/Database/Exhibit.cs
@@ -3,11 +3,9 @@
 public class Exhibit : Base
 {
     [Key] public long Id { get; init; }
-
     public required string Reference { get; init; }
     public required string Description { get; init; }
     public required DateTime DateTimeSeizedProduced { get; init; }
     public required string WhereSeizedProduced { get; init; }
     public required string SeizedBy { get; init; }
-    public ICollection<User> Users { get; } = new List<User>();
 }
